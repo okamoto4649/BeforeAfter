f2094765359865d3f61713e13c02a957f874d3bd
yuya okamoto <okam.vh@gmail.com>
2013-04-28 19:31:55 +0900

    test
    test comment

---
+++ src/yokamoto/apps/BeforeAfterActivity.java
@@ -0,0 +1,13 @@
+package yokamoto.apps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeforeAfterActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
